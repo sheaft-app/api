fe045e19f82f6177b5f5484c4d55267458cfe2c2
Noël Mugnier <noel.mugnier@outlook.com>
2020-08-19 09:35:09 +0200

    New - Ajout de l'impersonification pour le support et admin

--- Sheaft.Manage/Controllers/AccountController.cs
+++ Sheaft.Manage/Controllers/AccountController.cs
@@ -1,17 +1,38 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Sheaft.Core.Extensions;
+using Sheaft.Domain.Models;
+using Sheaft.Infrastructure.Interop;
+using Sheaft.Models.ViewModels;
 
 namespace Sheaft.Manage.Controllers
 {
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IConfigurationProvider _configurationProvider;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(
+            IAppDbContext context,
+            IMapper mapper,
+            IConfigurationProvider configurationProvider,
+            ILogger<AccountController> logger)
         {
+            _context = context;
+            _mapper = mapper;
+            _configurationProvider = configurationProvider;
             _logger = logger;
         }
 
@@ -21,10 +42,67 @@ namespace Sheaft.Manage.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Impersonate(CancellationToken token)
+        {
+            var id = HttpContext.Request.ImpersonificationId();
+            if (id.HasValue)
+            {
+                ViewBag.User = await _context.Users
+                    .Where(c => c.Id == id.Value)
+                    .ProjectTo<UserViewModel>(_configurationProvider)
+                    .SingleOrDefaultAsync(token);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ImpersonateByEmail(string email, CancellationToken token)
+        {
+            var users = await _context.Users
+                .Where(c => 
+                    (c.Email.Contains(email) && c.UserType == Interop.Enums.UserKind.Consumer) || 
+                    (c.Company != null && c.Company.Email.Contains(email) && c.UserType == Interop.Enums.UserKind.Owner))
+                .ProjectTo<UserViewModel>(_configurationProvider)
+                .ToListAsync(token);
+
+            return View("Impersonate", users);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ImpersonateById(Guid id, CancellationToken token)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(c => c.Id == id, token);
+            Impersonate(user);
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RemoveImpersonification()
+        {
+            HttpContext.Response.Cookies.Delete("Sheaft.Impersonating.Id");
+            HttpContext.Response.Cookies.Delete("Sheaft.Impersonating.Name");
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task Logout()
         {
+            RemoveImpersonification();
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
+        }
+
+        private void Impersonate(User user)
+        {
+            var name = user.Company != null ? user.Company.Name : $"{user.FirstName} {user.LastName}";
+            HttpContext.Response.Cookies.Append("Sheaft.Impersonating.Id", user.Id.ToString("N"), new CookieOptions { Secure = true, HttpOnly = true, SameSite = SameSiteMode.Strict });
+            HttpContext.Response.Cookies.Append("Sheaft.Impersonating.Name", name, new CookieOptions { Secure = true, HttpOnly = true, SameSite = SameSiteMode.Strict });
         }
     }
 }
---
+++ Sheaft.Manage/Controllers/ManageController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Sheaft.Core.Extensions;
+using Sheaft.Core.Security;
+using Sheaft.Infrastructure.Interop;
+using Sheaft.Interop.Enums;
+using Sheaft.Models.ViewModels;
+using Sheaft.Options;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sheaft.Manage.Controllers
+{
+    public class ManageController : Controller
+    {
+        protected readonly IAppDbContext _context;
+        protected readonly IMapper _mapper;
+        protected readonly IMediator _mediatr;
+        protected readonly RoleOptions _roleOptions;
+        protected readonly IConfigurationProvider _configurationProvider;
+
+        public ManageController(
+            IAppDbContext context,
+            IMapper mapper,
+            IOptionsSnapshot<RoleOptions> roleOptions,
+            IMediator mediatr,
+            IConfigurationProvider configurationProvider)
+        {
+            _context = context;
+            _mapper = mapper;
+            _mediatr = mediatr;
+            _roleOptions = roleOptions.Value;
+            _configurationProvider = configurationProvider;
+        }
+
+        protected async Task<List<TagViewModel>> GetTags(CancellationToken token)
+        {
+            return await _context.Tags
+                .AsNoTracking()
+                .ProjectTo<TagViewModel>(_configurationProvider)
+                .ToListAsync(token);
+        }
+
+        protected async Task<RequestUser> GetRequestUser(CancellationToken token)
+        {
+            var requestUser = User.ToIdentityUser(HttpContext.TraceIdentifier);
+
+            var cookie = HttpContext.Request.ImpersonificationId();
+            if (cookie.HasValue)
+            {
+                var user = await _context.Users.SingleOrDefaultAsync(u => u.Id == cookie.Value, token);
+                if(user != null)
+                {
+                    var name = user.Company != null ? user.Company.Name : $"{user.FirstName} {user.LastName}";
+                    var email = user.Company != null ? user.Company.Email : user.Email;
+                    var roles = user.Company != null ? 
+                        new List<string>()
+                        {
+                            _roleOptions.Owner.Value,
+                            user.Company.Kind == ProfileKind.Producer ? _roleOptions.Producer.Value : _roleOptions.Store.Value
+                        } :
+                        new List<string>()
+                        {
+                            _roleOptions.Consumer.Value
+                        };
+
+                    requestUser = new RequestUser(user.Id, name, email, roles, user.Company?.Id, HttpContext.TraceIdentifier, User.TryGetUserId(), User.GetName());
+                }
+            }
+
+            return requestUser;
+        }
+
+    }
+}
