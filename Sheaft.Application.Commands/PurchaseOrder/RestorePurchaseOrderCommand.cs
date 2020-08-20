82033d924b45c1443a8b9faa4e92138b89655be6
Noël Mugnier <noel.mugnier@outlook.com>
2020-08-20 14:25:44 +0200

    Change - Prise en compte du restore purchase orders

---
+++ Sheaft.Application.Commands/PurchaseOrder/RestorePurchaseOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sheaft.Application.Commands
+{
+    public class RestorePurchaseOrderCommand : Command<bool>
+    {
+        public RestorePurchaseOrderCommand(Interop.IRequestUser user) : base(user)
+        {
+        }
+
+        public Guid Id { get; set; }
+    }
+}
--- Sheaft.Manage/Controllers/PurchaseOrdersController.cs
+++ Sheaft.Manage/Controllers/PurchaseOrdersController.cs
@@ -120,16 +120,48 @@ namespace Sheaft.Manage.Controllers
         }
 
         [HttpPost]
-        [Authorize(Policy = "Admin")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(Guid id, CancellationToken token)
         {
             var entity = await _context.PurchaseOrders.SingleOrDefaultAsync(c => c.Id == id, token);
+            var reference = entity.Reference;
 
-            _context.Remove(entity);
-            await _context.SaveChangesAsync(token);
+            var requestUser = await GetRequestUser(token);
+            var result = await _mediatr.Send(new DeletePurchaseOrderCommand(requestUser)
+            {
+                Id = id
+            }, token);
 
-            TempData["Removed"] = entity.Reference;
+            if (!result.Success)
+            {
+                ModelState.AddModelError("", result.Exception.Message);
+                return RedirectToAction("Index");
+            }
+
+            TempData["Removed"] = reference;
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Restore(Guid id, CancellationToken token)
+        {
+            var entity = await _context.PurchaseOrders.SingleOrDefaultAsync(c => c.Id == id, token);
+            var name = entity.Reference;
+
+            var requestUser = await GetRequestUser(token);
+            var result = await _mediatr.Send(new RestorePurchaseOrderCommand(requestUser)
+            {
+                Id = id
+            }, token);
+
+            if (!result.Success)
+            {
+                ModelState.AddModelError("", result.Exception.Message);
+                return RedirectToAction("Index");
+            }
+
+            TempData["Restored"] = name;
             return RedirectToAction("Index");
         }
     }
