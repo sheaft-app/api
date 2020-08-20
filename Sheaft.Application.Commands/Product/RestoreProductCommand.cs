5db4f732134f597383f133df3c7cf4c177a2219d
Noël Mugnier <noel.mugnier@outlook.com>
2020-08-20 14:25:33 +0200

    Change - Prise en compte du restore products

---
+++ Sheaft.Application.Commands/Product/RestoreProductCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sheaft.Application.Commands
+{
+    public class RestoreProductCommand : Command<bool>
+    {
+        public RestoreProductCommand(Interop.IRequestUser user) : base(user)
+        {
+        }
+
+        public Guid Id { get; set; }
+    }
+}
--- Sheaft.Manage/Controllers/ProductsController.cs
+++ Sheaft.Manage/Controllers/ProductsController.cs
@@ -145,16 +145,48 @@ namespace Sheaft.Manage.Controllers
         }
 
         [HttpPost]
-        [Authorize(Policy = "Admin")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(Guid id, CancellationToken token)
         {
             var entity = await _context.Products.SingleOrDefaultAsync(c => c.Id == id, token);
+            var name = entity.Name;
 
-            _context.Remove(entity);
-            await _context.SaveChangesAsync(token);
+            var requestUser = await GetRequestUser(token);
+            var result = await _mediatr.Send(new DeleteProductCommand(requestUser)
+            {
+                Id = id
+            }, token);
 
-            TempData["Removed"] = entity.Name;
+            if (!result.Success)
+            {
+                ModelState.AddModelError("", result.Exception.Message);
+                return RedirectToAction("Index");
+            }
+
+            TempData["Removed"] = name;
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Restore(Guid id, CancellationToken token)
+        {
+            var entity = await _context.Products.SingleOrDefaultAsync(c => c.Id == id, token);
+            var name = entity.Name;
+
+            var requestUser = await GetRequestUser(token);
+            var result = await _mediatr.Send(new RestoreProductCommand(requestUser)
+            {
+                Id = id
+            }, token);
+
+            if (!result.Success)
+            {
+                ModelState.AddModelError("", result.Exception.Message);
+                return RedirectToAction("Index");
+            }
+
+            TempData["Restored"] = name;
             return RedirectToAction("Index");
         }
 
