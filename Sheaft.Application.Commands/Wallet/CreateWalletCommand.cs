85eb475e7eed3eeb549b42e97e2d8bb6be5727c7
Noël Mugnier <noel.mugnier@outlook.com>
2020-09-10 17:13:45 +0200

    New - Ajout d'une command pour créer un wallet

---
+++ Sheaft.Application.Commands/Wallet/CreateWalletCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Sheaft.Core;
+using Sheaft.Interop.Enums;
+
+namespace Sheaft.Application.Commands
+{
+    public class CreateWalletCommand : Command<Guid>
+    {
+        [JsonConstructor]
+        public CreateWalletCommand(RequestUser requestUser) : base(requestUser)
+        {
+        }
+
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public WalletKind Kind { get; set; }
+    }
+}
