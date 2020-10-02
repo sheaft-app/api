48ef61509acb2b09758ad68cf0a4b2e568fc18a8
Noël Mugnier <noel.mugnier@outlook.com>
2020-10-02 14:33:44 +0200

    Update - Utilisation de hangfire pour les notification signalr

--- Sheaft.Web.Signalr/Startup.cs
+++ Sheaft.Web.Signalr/Startup.cs
@@ -1,20 +1,34 @@
+using Hangfire;
 using IdentityModel;
+using MangoPay.SDK;
+using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SendGrid;
+using Sheaft.Application.Commands;
+using Sheaft.Application.Events;
+using Sheaft.Application.Handlers;
 using Sheaft.Application.Interop;
+using Sheaft.Infrastructure.Persistence;
 using Sheaft.Infrastructure.Services;
 using Sheaft.Options;
 using Sheaft.Signalr.Controllers;
+using Sheaft.Web.Signalr;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -36,14 +50,119 @@ namespace Sheaft.Signalr
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            var pspSettings = Configuration.GetSection(PspOptions.SETTING);
+            var jobsDatabaseSettings = Configuration.GetSection(JobsDatabaseOptions.SETTING);
+            var databaseSettings = Configuration.GetSection(AppDatabaseOptions.SETTING);
+            var sendgridSettings = Configuration.GetSection(SendgridOptions.SETTING);
+
+            services.Configure<SendgridOptions>(sendgridSettings);
+            services.Configure<JobsDatabaseOptions>(jobsDatabaseSettings);
+            services.Configure<AppDatabaseOptions>(databaseSettings);
+            services.Configure<PspOptions>(pspSettings);
+            services.Configure<ApiOptions>(Configuration.GetSection(ApiOptions.SETTING));
+            services.Configure<ServiceBusOptions>(Configuration.GetSection(ServiceBusOptions.SETTING));
+
+            services.AddHttpClient();
+
+            var sendgridConfig = sendgridSettings.Get<SendgridOptions>();
+            services.AddScoped<ISendGridClient, SendGridClient>(c => new SendGridClient(sendgridConfig.ApiKey));
+
+            var databaseConfig = databaseSettings.Get<AppDatabaseOptions>();
+            services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+            {
+                options.UseLazyLoadingProxies();
+                options.UseSqlServer(databaseConfig.ConnectionString, x => x.UseNetTopologySuite());//.EnableRetryOnFailure(5, TimeSpan.FromSeconds(5), null));
+            }, ServiceLifetime.Scoped);
+
+            services.AddScoped<IDapperContext, DapperContext>();
+            services.AddScoped<IIdentifierService, IdentifierService>();
+            services.AddScoped<IQueueService, QueueService>();
+            services.AddScoped<IBlobService, BlobService>();
+            services.AddScoped<IEmailService, EmailService>();
+            services.AddScoped<ISignalrService, SignalrService>();
+            services.AddScoped<IPictureService, PictureService>();
+            services.AddScoped<IPspService, PspService>();
+            services.AddScoped<ISheaftMediatr, SheaftMediatr>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IFeesService, FeesService>();
+
+            services.AddMediatR(new List<Assembly>() { typeof(RegisterStoreCommand).Assembly, typeof(UserPointsCreatedEvent).Assembly, typeof(UserCommandsHandler).Assembly }.ToArray());
+            services.AddScoped<IBackgroundJobClient, BackgroundJobClient>();
+            services.AddScoped<ISheaftHangfireBridge, SheaftHangfireBridge>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddApplicationInsightsTelemetry();
+            services.AddOptions();
+            services.AddLocalization(ops => ops.ResourcesPath = "Resources");
+
+            var pspOptions = pspSettings.Get<PspOptions>();
+            services.AddScoped<MangoPayApi>(c => new MangoPayApi
+            {
+                Config = new MangoPay.SDK.Core.Configuration
+                {
+                    BaseUrl = pspOptions.ApiUrl,
+                    ClientId = pspOptions.ClientId,
+                    ClientPassword = pspOptions.ApiKey
+                }
+            });
+
+            services.Configure<RequestLocalizationOptions>(
+                opts =>
+                {
+                    var supportedCultures = new List<CultureInfo>
+                    {
+                        new CultureInfo("en"),
+                        new CultureInfo("fr")
+                    };
+
+                    opts.DefaultRequestCulture = new RequestCulture("en", "fr");
+                    opts.SupportedCultures = supportedCultures;
+                    opts.SupportedUICultures = supportedCultures;
+                });
+
+            services.AddLogging(config =>
+            {
+                config.ClearProviders();
+
+                config.AddConfiguration(Configuration.GetSection("Logging"));
+                config.AddDebug();
+                config.AddEventSourceLogger();
+
+                if (Env.IsDevelopment())
+                {
+                    config.AddConsole();
+                }
+            });
+
+            services.AddLogging(config =>
+            {
+                config.ClearProviders();
+
+                config.AddConfiguration(Configuration.GetSection("Logging"));
+                config.AddDebug();
+                config.AddEventSourceLogger();
+                config.AddApplicationInsights();
+
+                if (Env.IsDevelopment())
+                {
+                    config.AddConsole();
+                }
+            });
+
+            var jobsDatabaseConfig = jobsDatabaseSettings.Get<JobsDatabaseOptions>();
+            services.AddHangfire(configuration =>
+            {
+                configuration.UseSqlServerStorage(jobsDatabaseConfig.ConnectionString);
+                configuration.UseMediatR();
+            });
+
             var authSettings = Configuration.GetSection(AuthOptions.SETTING);
             services.Configure<AuthOptions>(authSettings);
 
             var corsSettings = Configuration.GetSection(CorsOptions.SETTING);
             services.Configure<CorsOptions>(corsSettings);
-
-            services.Configure<StorageOptions>(Configuration.GetSection(StorageOptions.SETTING));
-            services.Configure<ServiceBusOptions>(Configuration.GetSection(ServiceBusOptions.SETTING));
 
             var corsConfig = corsSettings.Get<CorsOptions>();
             services.AddCors(options =>
@@ -57,13 +176,7 @@ namespace Sheaft.Signalr
                         .AllowCredentials();
                 });
             });
-
-            services.AddSingleton(Configuration);
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            services.AddScoped<IQueueService, QueueService>();
-
-            services.AddHttpClient();
+                        
             services.AddAuthorization();
 
             var authConfig = authSettings.Get<AuthOptions>();
@@ -123,28 +236,10 @@ namespace Sheaft.Signalr
 
             services.AddSingleton<IUserIdProvider, NameUserIdProvider>();
 
-            services.AddApplicationInsightsTelemetry();
-            services.AddOptions();
-
             services.AddMvc(option => option.EnableEndpointRouting = false)
                 .SetCompatibilityVersion(CompatibilityVersion.Latest);
 
             services.AddSignalR();
-
-            services.AddLogging(config =>
-            {
-                config.ClearProviders();
-
-                config.AddConfiguration(Configuration.GetSection("Logging"));
-                config.AddDebug();
-                config.AddEventSourceLogger();
-                config.AddApplicationInsights();
-
-                if (Env.IsDevelopment())
-                {
-                    config.AddConsole();
-                }
-            });
         }
 
         public void Configure(IApplicationBuilder app)
