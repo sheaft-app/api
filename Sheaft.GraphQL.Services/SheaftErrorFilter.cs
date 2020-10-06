e95bf9c96e45040d967536ffd4de4e414bbdc565
Noël Mugnier <noel.mugnier@outlook.com>
2020-10-06 21:03:19 +0200

    Update - Modification du message d'erreur par défaut

--- Sheaft.GraphQL.Services/SheaftErrorFilter.cs
+++ Sheaft.GraphQL.Services/SheaftErrorFilter.cs
@@ -23,29 +23,32 @@ namespace Sheaft.GraphQL.Services
         public IError OnError(IError error)
         {
             error = error.AddExtension("RequestIdentifier", _httpContextAccessor.HttpContext.TraceIdentifier);
-
+            var message = "Une erreur inattendue est survenue, veuillez renouveler votre demande. Si l'erreur persiste, contactez notre support.";
             if (error.Exception != null && error.Exception is SheaftException)
             {
                 var exc = error.Exception as SheaftException;
 
-                error = error.WithCode(exc.Kind.ToString("G"));
-                error = error.WithMessage(_localizer[exc.Kind.ToString("G")]);
-
                 if (exc.Error.HasValue)
-                {
-                    error = error.AddExtension(exc.Kind.ToString("G"), _localizer[exc.Error.Value.ToString("G"), exc.Params ?? new object[] { }]);
-                }
+                    message = _localizer[exc.Error.Value.ToString("G"), exc.Params ?? new object[] { }];
+
+                error = error.WithCode(exc.Kind.ToString("G"));
+                error = error.WithMessage(message);
+
+                error = error.AddExtension(exc.Kind.ToString("G"), message);
 
                 _logger.LogError(exc, $"{error.Code} -  {error.Message}", error);
             }
             else
             {
+                error = error.WithMessage(message);
                 _logger.LogError(error.Exception, $"{error.Code} -  {error.Message}", error);
             }
 
             if (error.Code == "AUTH_NOT_AUTHORIZED")
             {
                 error = error.WithCode(ExceptionKind.Unauthorized.ToString("G"));
+                error = error.WithMessage(_localizer[ExceptionKind.Unauthorized.ToString("G")]);
+
                 error = error.AddExtension(ExceptionKind.Unauthorized.ToString("G"), _localizer[ExceptionKind.Unauthorized.ToString("G")]);
             }
 
