b42389a124aab44c7e62b2c29367de1cfadb3b2f
Noël Mugnier <noel.mugnier@outlook.com>
2020-10-06 21:17:07 +0200

    Fix - Correction de l'api de register form contact

--- Sheaft.Web.Api/Controllers/ContactController.cs
+++ Sheaft.Web.Api/Controllers/ContactController.cs
@@ -31,6 +31,16 @@ namespace Sheaft.Web.Api.Controllers
         public IActionResult RegisterNewsletter([FromForm] RegisterNewsletterInput model)
         {
             var origin = Request.Headers.FirstOrDefault(c => c.Key == HeaderNames.Origin).Value;
+
+            if (string.IsNullOrWhiteSpace(model.Email))
+                return new RedirectResult(origin + "?error=L'adresse mail est requise");
+
+            if (string.IsNullOrWhiteSpace(model.FirstName))
+                return new RedirectResult(origin + "?error=Votre prénom est requis");
+
+            if (string.IsNullOrWhiteSpace(model.Role))
+                return new RedirectResult(origin + "?error=Vous devez indiquer à quel type de profil vous correspondez.");
+
             _mediatr.Post(new CreateContactCommand(new RequestUser("contact-user", HttpContext.TraceIdentifier)) { FirstName = model.FirstName, Role = model.Role, Email = model.Email });            
             var url = origin + _landingOptions.NewsletterUrl;
             return new RedirectResult(url);
