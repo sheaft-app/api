eb4b070aa614f2380b7fc98f6872ccb4b3b5f87f
Noël Mugnier <noel.mugnier@outlook.com>
2021-01-10 17:27:50 +0100

    Fix - Correction du prix de la validation de documents producteur

--- Sheaft.Options/PspOptions.cs
+++ Sheaft.Options/PspOptions.cs
@@ -20,7 +20,7 @@ namespace Sheaft.Options
         public decimal FixedAmount { get; set; } = 0.18m;
         public decimal Percent { get; set; } = 0.018m;
         public decimal VatPercent { get; set; } = 0.17m;
-        public decimal ProducerFees { get; set; } = 2.5m;
+        public decimal ProducerFees { get; set; } = 2.93m;
         public bool SkipIdempotencyKey { get; set; } = false;
         public string Token => $"{ClientId}:{ApiKey}".Base64Encode();
     }
