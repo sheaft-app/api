7d1561c35ff13bd6cb79b90985f5338672d9051c
Noël Mugnier <noel.mugnier@outlook.com>
2021-04-10 17:20:00 +0200

    Fix - Retourner uniquement les deliveries qui se terminent après la date du jour

--- Sheaft.Domain/TimeRange.cs
+++ Sheaft.Domain/TimeRange.cs
@@ -12,8 +12,8 @@ namespace Sheaft.Domain
         protected TimeRange(Guid id, DateTimeOffset from, DateTimeOffset to)
         {
             Id = id;
-            ClosedFrom = from;
-            ClosedTo = to;
+            ClosedFrom = GetFromDate(@from.LocalDateTime);
+            ClosedTo = GetToDate(@from.LocalDateTime, to.LocalDateTime);
         }
 
         public Guid Id { get; private set; }
@@ -24,8 +24,18 @@ namespace Sheaft.Domain
 
         public void ChangeClosedDates(DateTimeOffset from, DateTimeOffset to)
         {
-            ClosedFrom = from;
-            ClosedTo = to;
+            ClosedFrom = GetFromDate(@from.LocalDateTime);
+            ClosedTo = GetToDate(@from.LocalDateTime, to.LocalDateTime);
+        }
+
+        private static DateTime GetToDate(DateTimeOffset @from, DateTimeOffset to)
+        {
+            return @from.Date == to.Date ? new DateTime(to.Year, to.Month, to.Day, 23, 59, 59) : new DateTime(to.Year, to.Month, to.Day, 0, 0, 0);
+        }
+
+        private static DateTime GetFromDate(DateTimeOffset @from)
+        {
+            return new DateTime(@from.Year, @from.Month, @from.Day, 0, 0, 0);
         }
     }
 }
