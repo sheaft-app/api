c6362fe4e418fb732bd842020092fadd8895ba1a
Noël Mugnier <noel.mugnier@outlook.com>
2021-05-28 15:40:01 +0200

    New - Ajout d'un projet pour mettre à jour les jpg to png sur le storage

---
+++ Sheaft.Storage.Updater/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+namespace Sheaft.Storage.Updater
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {  
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+            var containerClient =  new BlobContainerClient(config["ConnectionString"], "pictures");
+            
+            var success = true;
+            await foreach (var blob in containerClient.GetBlobsAsync(prefix: "users", cancellationToken: CancellationToken.None))
+            {
+                if (blob.Name.Contains("/profile/") || !blob.Name.Contains(".jpg")) 
+                    continue;
+                
+                var destBlob = containerClient.GetBlobClient(blob.Name.Replace(".jpg", ".png"));
+                var copy = await destBlob.StartCopyFromUriAsync(new Uri(containerClient.Uri + "/" + blob.Name),
+                    cancellationToken: CancellationToken.None);
+                var copyResponse = await copy.WaitForCompletionAsync();
+
+                var response =
+                    await containerClient.DeleteBlobAsync(blob.Name, cancellationToken: CancellationToken.None);
+                if (response.Status >= 400)
+                    success = false;
+            }
+            
+            await foreach (var blob in containerClient.GetBlobsAsync(prefix: "products", cancellationToken: CancellationToken.None))
+            {
+                if (!blob.Name.Contains(".jpg")) 
+                    continue;
+                
+                var destBlob = containerClient.GetBlobClient(blob.Name.Replace(".jpg", ".png"));
+                var copy = await destBlob.StartCopyFromUriAsync(new Uri(containerClient.Uri + "/" + blob.Name),
+                    cancellationToken: CancellationToken.None);
+                var copyResponse = await copy.WaitForCompletionAsync();
+
+                var response =
+                    await containerClient.DeleteBlobAsync(blob.Name, cancellationToken: CancellationToken.None);
+                if (response.Status >= 400)
+                    success = false;
+            }
+            
+            await foreach (var blob in containerClient.GetBlobsAsync(prefix: "tags", cancellationToken: CancellationToken.None))
+            {
+                if (!blob.Name.Contains(".jpg")) 
+                    continue;
+                
+                var destBlob = containerClient.GetBlobClient(blob.Name.Replace(".jpg", ".png"));
+                var copy = await destBlob.StartCopyFromUriAsync(new Uri(containerClient.Uri + "/" + blob.Name),
+                    cancellationToken: CancellationToken.None);
+                var copyResponse = await copy.WaitForCompletionAsync();
+
+                var response =
+                    await containerClient.DeleteBlobAsync(blob.Name, cancellationToken: CancellationToken.None);
+                if (response.Status >= 400)
+                    success = false;
+            }
+        }
+    }
+}
