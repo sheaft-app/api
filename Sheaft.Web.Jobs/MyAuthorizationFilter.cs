ff720bb94fd12bf74b5e7f14d3cd87762aa6a9e4
Noël Mugnier <noel.mugnier@outlook.com>
2021-07-05 20:19:08 +0200

    Update - Mise à jour de Hangfire

--- Sheaft.Web.Jobs/MyAuthorizationFilter.cs
+++ Sheaft.Web.Jobs/MyAuthorizationFilter.cs
@@ -3,6 +3,9 @@ using Hangfire.Dashboard;
 using Hangfire.States;
 using Hangfire.Storage;
 using System;
+using Hangfire.Annotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Sheaft.Options;
 
@@ -25,6 +28,20 @@ namespace Sheaft.Web.Jobs
 
             return httpContext.User.Identity.IsAuthenticated && (httpContext.User.IsInRole(_roleOptions.Admin.Value) ||
                                                                  httpContext.User.IsInRole(_roleOptions.Support.Value));
+        }
+    }
+    
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter {
+        private readonly string _policyName;
+
+        public HangfireAuthorizationFilter(string policyName) {
+            _policyName = policyName;
+        }
+
+        public bool Authorize([NotNull] DashboardContext context) {
+            var httpContext = context.GetHttpContext();
+            var authService = httpContext.RequestServices.GetRequiredService<IAuthorizationService>();
+            return authService.AuthorizeAsync(httpContext.User, _policyName).ConfigureAwait(false).GetAwaiter().GetResult().Succeeded;
         }
     }
 }
