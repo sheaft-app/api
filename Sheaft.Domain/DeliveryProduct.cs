acfe21b109fdbbb5b6e8a7988ad7dbbc87683791
Noël Mugnier <noel.mugnier@outlook.com>
2021-07-07 19:34:24 +0200

    Fix - Correction du template de BL et de la quantité des products returned

--- Sheaft.Domain/DeliveryProduct.cs
+++ Sheaft.Domain/DeliveryProduct.cs
@@ -18,12 +18,13 @@ namespace Sheaft.Domain
             : base(product, quantity)
         {
             RowKind = kind;
+            SetQuantity(quantity);
         }
 
         public ModificationKind RowKind { get; private set; }
         public Guid DeliveryId { get; private set; }
         
-        protected override void SetQuantity(int quantity)
+        protected sealed override void SetQuantity(int quantity)
         {
             if(RowKind is ModificationKind.ToDeliver or ModificationKind.Excess)
                 Quantity = quantity;
