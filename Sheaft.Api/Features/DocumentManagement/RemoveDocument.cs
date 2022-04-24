ffd39f27fe19c35138cf8184d7ea224d526e7764
Noël Mugnier <noel.mugnier@outlook.com>
2022-04-24 10:01:00 +0200

    Expose remove document

---
+++ Sheaft.Api/Features/DocumentManagement/RemoveDocument.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Sheaft.Application;
+using Sheaft.Application.DocumentManagement;
+using Sheaft.Application.OrderManagement;
+using Sheaft.Domain;
+
+namespace Sheaft.Api.DocumentManagement;
+
+[Route(Routes.DOCUMENTS)]
+public class RemoveDocument : Feature
+{
+    public RemoveDocument(ISheaftMediator mediator)
+        : base(mediator)
+    {
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Post([FromRoute] string id, CancellationToken token)
+    {
+        var result = await Mediator.Execute(new RemoveDocumentCommand(new DocumentId(id)), token);
+        return HandleCommandResult(result);
+    }
+}
